c9a8fbcf60c270aa9d21147ee4a4430ff11c010a
Dr.M.Sajid <geosajid@gmail.com>
2023-10-12 21:17:23 +0800

    added Image processing

---
+++ ImageProcessing/app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from PIL import Image
+import PIL
+import matplotlib.pyplot as plt
+from PIL import ImageFilter, ImageEnhance
+
+
+image = Image.open("../../data/birds.jpg")
+
+
+fig = plt.figure()
+
+
+def ploting():
+    plt.imshow(image)
+    plt.axis("off")
+    st.pyplot(fig)
+
+
+options = st.selectbox(
+    "Select the processing option",
+    [
+        "Show Image",
+        "rotate Image",
+        "Create Thumbnail",
+        "Crop Image",
+        "Merging",
+        "flipLR",
+        "flip270",
+        "Black and White",
+    ],
+)
+
+
+if options == "Show Image":
+    image = Image.open("../../data/birds.jpg")
+    ploting()
+
+elif options == "rotate Image":
+    image = image.rotate(180)
+    ploting()
+
+elif options == "create Thumbnails":
+    size = 50, 50
+    image.thumbnail(size, resample=PIL.Image.NEAREST)
+    image.save("thumbnail.png")
+    plt.grid("both")
+    ploting()
+
+elif options == "Crop Image":
+    box = (50, 100, 200, 200)
+    image = image.crop(box)
+    ploting()
+
+elif options == "Merging":
+    position = (0, 300)
+    logo = Image.open("../../data/icon.png")
+    image.paste(logo, position, logo)
+    ploting()
+
+elif options == "flipLR":
+    image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    ploting()
+
+elif options == "flip270":
+    image = image.transpose(Image.ROTATE_270)
+    ploting()
+
+elif options == "Black and White":
+    image = image.convert("1")
+    ploting()
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Steamlit
 
-This streamlit python package testing
-
-This Streamlit testing application is written by Dr.M.Sajid
+Streamlit python user friendly Graphical User Interface
