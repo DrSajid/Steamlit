12cbc676d4049cd60c646cc626a6b9a0148452d5
Dr.M.Sajid <geosajid@gmail.com>
2023-10-15 13:27:56 +0800

    plotly animation and subplots

--- plotly/app.py
+++ plotly/app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
 
-import plotly.subplots as make_subplots
+
 
 
 from plotData import *
 
 
-plot = st.selectbox("Select a plot", ["Show data", "Pie Chart", "Donut Chart"])
+plot = st.selectbox(
+    "Select a plot",
+    ["Show data", "Pie Chart", "Donut Chart", "Scatter Plot", "Scatter with Columans", "Line Plot", "Bar plot", "Stack Barchar", "Animation", "Subplots"],
+)
 
 
 def main():
@@ -17,7 +20,27 @@ def main():
         pieChart()
     elif plot == "Donut Chart":
         Donut_Chart()
-
+    elif plot == "Scatter Plot":
+        st.header("Scatter Plot")
+        Scatter_Plot()
+    elif plot == "Scatter with Columans":
+        st.header("Scatter with Columans")
+        Scatter_with_Columans()
+    elif plot == "Line Plot":
+        st.header("Line Plot")
+        LinePlot()
+    elif plot == "Bar plot":
+        st.header("Bar plot")
+        Bar_plot()
+    elif plot == "Stack Barchar":
+        st.header("Stack Barchar")
+        Stack_Barchar()
+    elif plot == "Animation":
+        st.header("Animation")
+        Animate()
+    elif plot == "Subplots":
+        st.header("Subplots")
+        Subplots()
 
 if __name__ == "__main__":
     main()
--- plotly/plotData.py
+++ plotly/plotData.py
@@ -1,6 +1,7 @@
 from loadData import *
 import plotly.graph_objects as go
 import plotly.express as px
+import plotly.subplots as make_subplots
 
 
 def ShowData():
@@ -39,5 +40,164 @@ def Donut_Chart():
         values="secs_elapsed",
         names="device_type",
         color_discrete_sequence=px.colors.sequential.Cividis,
+    )
+    st.plotly_chart(fig)
+
+
+def Scatter_Plot():
+    fig = px.scatter(
+        df,
+        x="age",
+        y="secs_elapsed",
+        color="month_name_account_created",
+        size="secs_elapsed",
+        hover_data=["day_account_created"],
+        width=900,
+        height=600,
+        color_discrete_sequence=px.colors.sequential.Agsunset,
+        category_orders={
+            "month_name_account_created": [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ]
+        },
+    )
+    st.plotly_chart(fig)
+
+
+def Scatter_with_Columans():
+    fig = px.scatter(
+        df,
+        x="age",
+        y="secs_elapsed",
+        color="month_name_account_created",
+        size="secs_elapsed",
+        hover_data=["day_account_created"],
+        facet_col="signup_method",
+        width=900,
+        height=600,
+        color_discrete_sequence=px.colors.sequential.Agsunset,
+        category_orders={
+            "month_name_account_created": [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ]
+        },
+    )
+    st.plotly_chart(fig)
+
+
+def LinePlot():
+    dff = df.groupby("dayofyear_account_created")["user_id"].count().reset_index()
+    dff.columns = ["Day of Year", "Number of Accounts"]
+    fig = px.line(dff, x="Day of Year", y="Number of Accounts", width=700)
+    st.plotly_chart(fig)
+
+
+def Bar_plot():
+    dff = df.groupby("device_type")["secs_elapsed"].sum().reset_index()
+    fig = px.bar(
+        dff,
+        x="device_type",
+        y="secs_elapsed",
+        title="Seconds vs device type",
+        color="device_type",
+        width=1000,
+        height=700,
+        color_discrete_sequence=px.colors.qualitative.G10,
+    )
+    st.plotly_chart(fig)
+
+
+def Stack_Barchar():
+    dff = (
+        df.groupby(["device_type", "signup_method"])["secs_elapsed"].sum().reset_index()
+    )
+    fig = px.bar(
+        dff,
+        x="device_type",
+        y="secs_elapsed",
+        title="Secs vs device type",
+        color="signup_method",
+        color_discrete_sequence=px.colors.qualitative.D3,
+        width=900,
+        height=700,
+    )
+    st.plotly_chart(fig)
+
+
+def Animate():
+    fig = px.scatter(
+        df,
+        x="age",
+        y="secs_elapsed",
+        color="signup_method",
+        animation_frame="month_name_account_created",
+        size_max=40,
+        size="secs_elapsed",
+        hover_data=["day_account_created"],
+        width=900,
+        height=600,
+        color_discrete_sequence=px.colors.sequential.Agsunset,
+        category_orders={
+            "month_name_account_created": [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ]
+        },
+    )
+    st.plotly_chart(fig)
+
+
+def Subplots():
+    dff = df.groupby("signup_method")["secs_elapsed"].sum().reset_index()
+    fig = make_subplots.make_subplots(rows=1, cols=2)
+    fig.add_trace(
+        go.Bar(
+            x=dff["signup_method"],
+            y=df["secs_elapsed"],
+            marker=dict(color="SkyBlue"),
+        ),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Bar(
+            x=dff["signup_method"],
+            y=df["secs_elapsed"],
+            marker=dict(color="red"),
+        ),
+        row=1,
+        col=2,
     )
     st.plotly_chart(fig)
