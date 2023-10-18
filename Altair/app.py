6593fa308bb92dd6ec98165e77b6e08f8af20670
Dr.M.Sajid <geosajid@gmail.com>
2023-10-18 20:24:59 +0800

    implemented scatter, area and bar_chart

--- Altair/app.py
+++ Altair/app.py
@@ -2,12 +2,14 @@ import streamlit as st
 import pandas as pd
 
 
-
 from plotData import *
 
 
 def main():
-    page = st.sidebar.selectbox("Select Page", ["data", "Vaga lite", "Line Plot"])
+    page = st.sidebar.selectbox(
+        "Select Page",
+        ["data", "Vaga lite", "Line Plot", "Bar chart", "Scatter plot", "Area Plot"],
+    )
     if page == "data":
         st.write(df.head(10))
     elif page == "Vaga lite":
@@ -16,6 +18,15 @@ def main():
     elif page == "Line Plot":
         st.header("Line Plot")
         LinePlot()
+    elif page == "Bar chart":
+        st.header("Bar Chart")
+        BarChart()
+    elif page == "Scatter plot":
+        st.header("Scatter plot")
+        Scatter_plot()
+    elif page == "Area Plot":
+        st.header("Area Plot")
+        Area_plot()
 
 
 if __name__ == "__main__":
--- Altair/plotData.py
+++ Altair/plotData.py
@@ -51,3 +51,89 @@ def LinePlot():
     )
 
     st.altair_chart(line)
+
+
+def BarChart():
+    bar_data = df.sort_values(by="views", ascending=False)
+    bar_data = bar_data.head(20)
+
+    sort = st.checkbox("Sort", value=False, help="Sort data")
+
+    if sort:
+        chart = (
+            alt.Chart(bar_data)
+            .mark_bar()
+            .encode(x=alt.X("event:N", sort="-y"), y="views")
+            .properties(width=650, height=500)
+            .interactive()
+        )
+    else:
+        chart = (
+            alt.Chart(bar_data)
+            .mark_bar()
+            .encode(x="event", y="views")
+            .properties(width=650, height=500)
+            .interactive()
+        )
+    st.altair_chart(chart)
+
+
+def Scatter_plot():
+    toggle = st.checkbox(
+        "Toggle background", value=False, help="will change background"
+    )
+    if toggle:
+        chart = (
+            alt.Chart(df, background="maroon")
+            .mark_point()
+            .encode(
+                x="duration",
+                y="views",
+                size="languages",
+                color="languages",
+                tooltip=["duration", "views", "comments", "languages"],
+            )
+            .properties(width=650, height=500)
+            .interactive()
+        )
+    else:
+        chart = (
+            alt.Chart(df)
+            .mark_point()
+            .encode(
+                x="duration",
+                y="views",
+                size="languages",
+                color="languages",
+                tooltip=["duration", "views", "comments", "languages"],
+            )
+            .properties(width=650, height=500)
+            .interactive()
+        )
+    st.altair_chart(chart)
+
+
+def Area_plot():
+    df_copy = df[df["film_year"] == 2012]
+    df_copy["film_date"] = pd.to_datetime(df_copy["film_date"])
+
+    radio_button = st.radio("With or without line", ("line", "without line"))
+
+    if radio_button == "line":
+        chart = (
+            alt.Chart(df_copy)
+            .mark_area(color="maroon", line=True)
+            .encode(x="film_date", y="views")
+            .properties(width=650, height=500)
+            .interactive()
+        )
+    else:
+        chart = (
+            alt.Chart(df_copy)
+            .mark_area(color="maroon", line=False)
+            .encode(x="film_date", y="views")
+            .properties(width=650, height=500)
+            .interactive()
+        )
+
+    st.altair_chart(chart)
