cc61c81a0b71acbbb1987e23db04129063527912
Dr.M.Sajid <geosajid@gmail.com>
2023-10-25 20:21:34 +0800

    Natural Language Processing

---
+++ nlp/app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from transformers import pipeline
+
+option = st.selectbox(
+    "select an option",
+    [
+        "Classify it",
+        "Question Answering",
+        "Text generation",
+        "Recognistion",
+        "Summarize",
+    ],
+)
+
+if option == "Classify it":
+    text = st.text_area(label="Enter text")
+    if text:
+        classifier = pipeline("sentiment-analysis")
+        answer = classifier(text)
+        st.write(answer)
+elif option == "Question Answering":
+    q_a = pipeline("question-answering")
+    context = st.text_area(label="Enter context")
+    question = st.text_area(label=" Enter Question")
+    if context and question:
+        answer = q_a({"question": question, "context": context})
+        st.write(answer)
+
+elif option == "Text generation":
+    text = st.text_area(label="Enter context")
+    if text:
+        text_generator = pipeline("text-generation")
+        answer = text_generator(text)
+        st.write(answer)
+
+elif option == "Recognistion":
+    text = st.text_area(label="Enter text")
+    if text:
+        text_recognition = pipeline("ner")
+        answer = text_recognition(text)
+        st.write(answer)
+elif option == "Summarize":
+    text = st.text_area(label="Enter text")
+    if text:
+        text_summarize = pipeline("summarization")
+        answer = text_summarize(text, max_length=100, min_length=50)
+        st.write(answer)
